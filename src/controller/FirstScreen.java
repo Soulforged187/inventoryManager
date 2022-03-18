f2aa382ba70b8c0d6ab6485530965c14516b1221
Soulforged187 <danielhansenjones@gmail.com>
2022-03-18 12:05:50 -0600

    first commit for basic outline for inventory Manager

---
+++ src/controller/FirstScreen.java
@@ -0,0 +1,23 @@
+package controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FirstScreen implements Initializable{
+    public Label TheLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Controller Initialized");
+        TheLabel.setText("Initialized");
+    }
+
+    public void onButtonAction(ActionEvent actionEvent) {
+        System.out.println("I was clicked");
+
+        TheLabel.setText("I have been clicked");
+    }
+}
